129c609a62e4a94df43443fec6482718f7287d06
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2024-01-30 12:40:14 +0400

    product list api changes

--- app/Http/Resources/V2/ProductFilterCollection.php
+++ app/Http/Resources/V2/ProductFilterCollection.php
@@ -21,10 +21,8 @@ class ProductFilterCollection extends ResourceCollection
                 'name' => $data->name,
                 'sku' => $data->sku,
                 'thumbnail_image' => app('url')->asset($data->thumbnail_img),
-                // 'has_discount' => home_base_price($data, false) != home_discounted_base_price($data, false),
                 'stroked_price' => $priceData['original_price'],
                 'main_price' => $priceData['discounted_price'],
-                // 'price_high_low' => (float)explode('-', home_discounted_base_price($data, false))[0] == (float)explode('-', home_discounted_price($data, false))[1] ? format_price((float)explode('-', home_discounted_price($data, false))[0]) : "From " . format_price((float)explode('-', home_discounted_price($data, false))[0]) . " to " . format_price((float)explode('-', home_discounted_price($data, false))[1]),
                 'min_qty' => $data->min_qty,
                 'slug' => $data->slug,
                 'offer_tag' => $priceData['offer_tag']
